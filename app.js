9f45faea157e8358f959ad4e7bd92b571e850c73
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2022-12-27 23:07:24 -0300

    Add opção de números não inteiros

--- app.js
+++ app.js
@@ -6,13 +6,43 @@ let memory1;
 let memory2;
 let op;
 
+function resultOp() {
+    memory2 = screen.innerText
+        let result;
+    switch (op) {
+      case "sum":
+        result = parseFloat(memory1) + parseFloat(memory2);
+        op = "result";
+        break;
+      case "sub":
+        result = parseFloat(memory1) - parseFloat(memory2);
+        op = "result";
+        break;
+      case "mult":
+        result = parseFloat(memory1) * parseFloat(memory2);
+        op = "result";
+        break;
+      case "div":
+        result = parseFloat(memory1) / parseFloat(memory2);
+        op = "result";
+        break;
+      default:
+        result = 0;
+        break;
+    }
+    screen.innerText = result;
+}
+
 document.querySelectorAll(".key").forEach(function (key) {
   key.addEventListener("click", function (event) {
     const id = event.target.id;
     if (id == "number") {
       if (screen.innerText == 0) screen.innerText = event.target.innerText;
       else screen.innerText = screen.innerText + event.target.innerText;
-    } else if (id == "sum") {
+    } else if (id == 'dot'){
+        screen.innerText = screen.innerText + event.target.innerText;
+    }
+    else if (id == "sum") {
       memory1 = screen.innerText;
       screen.innerText = 0;
       op = "sum";
@@ -31,35 +61,14 @@ document.querySelectorAll(".key").forEach(function (key) {
     } else if (id == "del") {
       let aux = screen.innerText.split("");
       aux.pop();
-      if (aux.length == 0) {
+      if (aux.length == 0 || op == "result") {
         screen.innerText = 0;
       } else screen.innerText = aux.join("");
     } else if (id == "equal") {
-      memory2 = screen.innerText;
-      let result;
-      console.log(op);
-      switch (op) {
-        case "sum":
-          result = parseInt(memory1) + parseInt(memory2);
-            op = 'result'
-          break;
-        case "sub":
-          result = parseInt(memory1) - parseInt(memory2);
-            op = 'result'
-          break;
-        case "mult":
-          result = parseInt(memory1) * parseInt(memory2);
-            op = 'result'
-          break;
-        case "div":
-          result = parseInt(memory1) / parseInt(memory2);
-            op = 'result'
-          break;
-        default:
-            result = 0
-          break;
-      }
-      screen.innerText = result;
-    } else if (id == "reset") screen.innerText = 0;
+        resultOp();
+    } else if (id == "reset") {
+        screen.innerText = 0;
+        op = 'reset';
+    }
   });
 });
