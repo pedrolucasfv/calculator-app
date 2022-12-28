9912fa6af3e21bc0aae9f38de96816e7bacc8ed8
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2022-12-28 01:15:44 -0300

    consertando o posicionamento

--- app.js
+++ app.js
@@ -1,37 +1,62 @@
-const toggle = document.getElementsByClassName("toggleButton");
+const toggle = document.querySelector(".toggleButton");
 const numbers = document.querySelectorAll("#number");
 const screen = document.getElementById("screen");
-const del = document.getElementById("del");
+const themeOne = document.getElementById("theme-one");
+const themeTwo = document.getElementById("theme-two");
+const themeThree = document.getElementById("theme-three");
+
 let memory1;
 let memory2;
 let op;
 
 function resultOp() {
-    memory2 = screen.innerText
-        let result;
-    switch (op) {
-      case "sum":
-        result = parseFloat(memory1) + parseFloat(memory2);
-        op = "result";
-        break;
-      case "sub":
-        result = parseFloat(memory1) - parseFloat(memory2);
-        op = "result";
-        break;
-      case "mult":
-        result = parseFloat(memory1) * parseFloat(memory2);
-        op = "result";
-        break;
-      case "div":
-        result = parseFloat(memory1) / parseFloat(memory2);
-        op = "result";
-        break;
-      default:
-        result = 0;
-        break;
-    }
-    screen.innerText = result;
+  memory2 = screen.innerText;
+  let result;
+  switch (op) {
+    case "sum":
+      result = parseFloat(memory1) + parseFloat(memory2);
+      op = "result";
+      break;
+    case "sub":
+      result = parseFloat(memory1) - parseFloat(memory2);
+      op = "result";
+      break;
+    case "mult":
+      result = parseFloat(memory1) * parseFloat(memory2);
+      op = "result";
+      break;
+    case "div":
+      result = parseFloat(memory1) / parseFloat(memory2);
+      op = "result";
+      break;
+    default:
+      result = 0;
+      break;
+  }
+  screen.innerText = result;
 }
+
+themeOne.addEventListener("click", function (event) {
+  if (toggle.classList.contains("togglePosition2"))
+    toggle.classList.remove("togglePosition2");
+  else if (toggle.classList.contains("togglePosition3"))
+    toggle.classList.remove("togglePosition3");
+  toggle.classList.add("togglePosition1");
+});
+themeTwo.addEventListener("click", function (event) {
+  if (toggle.classList.contains("togglePosition1"))
+    toggle.classList.remove("togglePosition1");
+  else if (toggle.classList.contains("togglePosition3"))
+    toggle.classList.remove("togglePosition3");
+  toggle.classList.add("togglePosition2");
+});
+themeThree.addEventListener("click", function (event) {
+  if (toggle.classList.contains("togglePosition1"))
+    toggle.classList.remove("togglePosition1");
+  else if (toggle.classList.contains("togglePosition2"))
+    toggle.classList.remove("togglePosition2");
+  toggle.classList.add("togglePosition3");
+});
 
 document.querySelectorAll(".key").forEach(function (key) {
   key.addEventListener("click", function (event) {
@@ -39,10 +64,9 @@ document.querySelectorAll(".key").forEach(function (key) {
     if (id == "number") {
       if (screen.innerText == 0) screen.innerText = event.target.innerText;
       else screen.innerText = screen.innerText + event.target.innerText;
-    } else if (id == 'dot'){
-        screen.innerText = screen.innerText + event.target.innerText;
-    }
-    else if (id == "sum") {
+    } else if (id == "dot") {
+      screen.innerText = screen.innerText + event.target.innerText;
+    } else if (id == "sum") {
       memory1 = screen.innerText;
       screen.innerText = 0;
       op = "sum";
@@ -65,10 +89,10 @@ document.querySelectorAll(".key").forEach(function (key) {
         screen.innerText = 0;
       } else screen.innerText = aux.join("");
     } else if (id == "equal") {
-        resultOp();
+      resultOp();
     } else if (id == "reset") {
-        screen.innerText = 0;
-        op = 'reset';
+      screen.innerText = 0;
+      op = "reset";
     }
   });
 });
