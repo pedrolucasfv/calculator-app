5be9046e4cf73c3751bc450181eb3e305ebad03a
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2022-12-29 14:48:25 -0300

    separando os estilos

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const toggle = document.querySelector(".toggleButton");
 const numbers = document.querySelectorAll("#number");
-const screen = document.getElementById("screen");
 const themeOne = document.getElementById("theme-one");
 const themeTwo = document.getElementById("theme-two");
 const themeThree = document.getElementById("theme-three");
+
+const screenId = document.getElementById("screen");
+const screenClass = document.querySelector(".screen");
+
+const header = document.querySelector('.header');
+const keypad = document.querySelector('.keypad');
+const keys = document.querySelectorAll('.key');
 
 let memory1;
 let memory2;
 let op;
 
 function resultOp() {
-  memory2 = screen.innerText;
+  memory2 = screenId.innerText;
   let result;
   switch (op) {
     case "sum":
@@ -33,12 +39,15 @@ function resultOp() {
       result = 0;
       break;
   }
-  screen.innerText = result;
+  screenId.innerText = result;
 }
 
 themeOne.addEventListener("click", function (event) {
-  if (toggle.classList.contains("togglePosition2"))
+  if (toggle.classList.contains("togglePosition2")){
     toggle.classList.remove("togglePosition2");
+    keypad.classList.remove("theme2")
+  }
+
   else if (toggle.classList.contains("togglePosition3"))
     toggle.classList.remove("togglePosition3");
   toggle.classList.add("togglePosition1");
@@ -49,6 +58,12 @@ themeTwo.addEventListener("click", function (event) {
   else if (toggle.classList.contains("togglePosition3"))
     toggle.classList.remove("togglePosition3");
   toggle.classList.add("togglePosition2");
+  keypad.classList.add("theme3")
+  keys.forEach(function(key) {
+    key.classList.add("theme3")
+  })
+  screenClass.classList.add("theme3")
+  header.classList.add("theme3")
 });
 themeThree.addEventListener("click", function (event) {
   if (toggle.classList.contains("togglePosition1"))
@@ -58,40 +73,40 @@ themeThree.addEventListener("click", function (event) {
   toggle.classList.add("togglePosition3");
 });
 
-document.querySelectorAll(".key").forEach(function (key) {
+keys.forEach(function (key) {
   key.addEventListener("click", function (event) {
     const id = event.target.id;
     if (id == "number") {
-      if (screen.innerText == 0) screen.innerText = event.target.innerText;
-      else screen.innerText = screen.innerText + event.target.innerText;
+      if (screenId.innerText == 0) screenId.innerText = event.target.innerText;
+      else screenId.innerText = screenId.innerText + event.target.innerText;
     } else if (id == "dot") {
-      screen.innerText = screen.innerText + event.target.innerText;
+      screenId.innerText = screenId.innerText + event.target.innerText;
     } else if (id == "sum") {
-      memory1 = screen.innerText;
-      screen.innerText = 0;
+      memory1 = screenId.innerText;
+      screenId.innerText = 0;
       op = "sum";
     } else if (id == "sub") {
-      memory1 = screen.innerText;
-      screen.innerText = 0;
+      memory1 = screenId.innerText;
+      screenId.innerText = 0;
       op = "sub";
     } else if (id == "div") {
-      memory1 = screen.innerText;
-      screen.innerText = 0;
+      memory1 = screenId.innerText;
+      screenId.innerText = 0;
       op = "div";
     } else if (id == "mult") {
-      memory1 = screen.innerText;
-      screen.innerText = 0;
+      memory1 = screenId.innerText;
+      screenId.innerText = 0;
       op = "mult";
     } else if (id == "del") {
-      let aux = screen.innerText.split("");
+      let aux = screenId.innerText.split("");
       aux.pop();
       if (aux.length == 0 || op == "result") {
-        screen.innerText = 0;
-      } else screen.innerText = aux.join("");
+        screenId.innerText = 0;
+      } else screenId.innerText = aux.join("");
     } else if (id == "equal") {
       resultOp();
     } else if (id == "reset") {
-      screen.innerText = 0;
+      screenId.innerText = 0;
       op = "reset";
     }
   });
